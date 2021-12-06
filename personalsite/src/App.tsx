e3bd5ad8d55b3d7f522c3166a74a79e12ff842b3
Anthony Huang <anthill499@gmail.com>
2021-12-06 02:33:45 -0500

    snap

--- personalsite/src/App.tsx
+++ personalsite/src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import styles from "./App.module.scss";
+import About from "./about";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <section className={styles.about}>
+        <About />
+      </section>
+      <section className={styles.technologies}>Technologies</section>
+      <section className={styles.projects}>Projects</section>
+      <section className={styles.contact}>Contact</section>
     </div>
   );
 }
