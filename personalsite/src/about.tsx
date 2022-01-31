ad610c9459e27207deb3273cc16990af4d54e274
Anthony Huang <anthill499@gmail.com>
2022-01-31 13:48:51 -0500

    img button

--- personalsite/src/about.tsx
+++ personalsite/src/about.tsx
@@ -1,5 +1,6 @@
 import styles from "./styles/about.module.scss";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import profilePic from "./files/profilepictureTwo.jpg";
 const About = () => {
   const [currPosition, setCurrPosition] = useState(0);
   const ROLES = [
@@ -15,12 +16,12 @@ const About = () => {
     </span>,
   ];
 
-  useEffect(() => {
-    const incrementer = setInterval(incrementCurr, 3000);
-    return () => {
-      clearInterval(incrementer);
-    };
-  });
+  // useEffect(() => {
+  //   const incrementer = setInterval(incrementCurr, 3000);
+  //   return () => {
+  //     clearInterval(incrementer);
+  //   };
+  // });
 
   const incrementCurr = () => {
     if (currPosition === 3) {
@@ -29,12 +30,30 @@ const About = () => {
       setCurrPosition(currPosition + 1);
     }
   };
+
   return (
     <>
       <div className={styles.aboutContainer}>
+        <img
+          src={profilePic}
+          alt="no pic"
+          className={styles.profilePicture}
+          onClick={() => incrementCurr()}
+        />
         <div className={styles.about}>
           <h1>Hey I'm {ROLES[currPosition]}</h1>
-          <div>I am a </div>
+          <div className={styles.description}>
+            I am a full stack software engineer based in NYC. I started my
+            software development journey in June 2021 by attending App Academy,
+            an intensive full stack web development bootcamp. Currently, I
+            aspire to be more well-rounded as a software engineer with emphasis
+            on backend development and data structures and algorithms. My future
+            goals are to work in the blockchain and data visualization space.
+            <br />
+            <br />
+            If I'm not programming, you can find me producing music and going to
+            the gym.
+          </div>
         </div>
       </div>
     </>
